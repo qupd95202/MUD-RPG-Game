d909359831354811614fa56fe0545a614b308ed3
Micky Tsai <leonzerg@hotmail.com>
2021-09-09 13:25:51 +0800

    測試push

--- RPG/src/rpg/RPG.java
+++ RPG/src/rpg/RPG.java
@@ -5,6 +5,8 @@
  */
 package rpg;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mickytsai
@@ -15,9 +17,30 @@ public class RPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Character king = new Character();
-        king.setDef(5);
-        System.out.println(king.getDef());
+        Scanner sc = new Scanner(System.in);
+        //角色命名
+        Player newPlayer = new Player();
+        System.out.println("歡迎進入遊戲");
+        System.out.print("請輸入角色名稱->");
+        newPlayer.setName(sc.next());
+        System.out.println("你好" + newPlayer.getName());
+        //選擇起始武器
+        System.out.println("請選擇一個武器");
+        //進入隨機地圖
+        
+        
+        //進入事件
+        
+        
+        
+        //死亡或殺死一個王
+        
+        
+        //過關
     }
+    public void showList (){    
+        
+    }
+    
     
 }
