00f227383faabc39a10ca185540e1b72319b2728
qupd95202 <b05305035@ntu.edu.tw>
2021-09-09 13:19:01 +0800

    新增屬性class

--- RPG/src/rpg/Player.java
+++ RPG/src/rpg/Player.java
@@ -1,77 +1,64 @@
+import java.util.ArrayList;
+
 public class Player extends Character {
-    private int itemMaxmum;
-    private int weaponMaxmum;
-    private int equipmentMaxmum;
-    private int con;
-    private int maxExp;
 
-    public int getItemMaxmum() {
-        return itemMaxmum;
-    }
-
-    public void setItemMaxmum(int itemMaxmum) {
-        this.itemMaxmum = itemMaxmum;
-    }
-
-    public int getWeaponMaxmum() {
-        return weaponMaxmum;
-    }
-
-    public void setWeaponMaxmum(int weaponMaxmum) {
-        this.weaponMaxmum = weaponMaxmum;
-    }
-
-    public int getEquipmentMaxmum() {
-        return equipmentMaxmum;
-    }
-
-    public void setEquipmentMaxmum(int equipmentMaxmum) {
-        this.equipmentMaxmum = equipmentMaxmum;
-    }
-
-    public int getCon() {
-        return con;
-    }
-
-    public void setCon(int con) {
-        this.con = con;
-    }
-
-    public int getMaxExp() {
-        return maxExp;
-    }
-
-    public void setMaxExp(int maxExp) {
-        this.maxExp = maxExp;
-    }
+    private ArrayList<Drops> weaponList = new ArrayList<Drops>();
+    private ArrayList<Drops> equipmentList = new ArrayList<Drops>();
+    private ArrayList<Drops> itemList = new ArrayList<Drops>();
 
     public Player() {
-        itemMaxmum = 5;
-        weaponMaxmum = 1;
-        equipmentMaxmum = 1;
-        this.setMaxHp(5);
+        Ability initialAbility = new Ability();
+        initialAbility.setArmorMaxmum(1);
+        initialAbility.setItemMaxmum(5);
+        initialAbility.setWeaponMaxmum(1);
+        initialAbility.setMaxHp(5);
+        initialAbility.setHp(5);
+        initialAbility.setDef(5);
+        initialAbility.setDex(5);
+        initialAbility.setStr(5);
+        initialAbility.setHit(5);
         this.addDex(5);
-        this.setStr(5);
+        this.setStr(5);           //初始素質之設定
         this.setHit(5);
         this.setDef(5);
         con = this.getStr() * 6;
         this.setLV(1);
-
         this.setExp(0);
         maxExp = 10;
-    }
+        position = 0;  //一開始位在原點
+        equipmentWeight + {
+        }
 
-    public void lvelUp() {
-        if (getExp() >= getMaxExp()) {
-            setLV(getLV() + 1);//升等
-            setExp(getExp() - getMaxExp());//經驗值歸零+溢出
-            setMaxExp(getMaxExp() * 2);//最大經驗值變成原本兩倍
-            addStr(2);
-            addDex(2);
-            addHp(2);   //各項素質提升
-            addHit(2);
-            addDef(2);
-            lvelUp(); //判定有無可能一次升多等
+        public void lvelUp () {
+            if (getExp() >= getMaxExp()) {
+                setLV(getLV() + 1);//升等
+                setExp(getExp() - getMaxExp());//經驗值歸零+溢出
+                setMaxExp(getMaxExp() * 2);//最大經驗值變成原本兩倍
+                addStr(2);
+                addDex(2);
+                addHp(2);   //各項素質提升
+                addHit(2);
+                addDef(2);
+                lvelUp(); //判定有無可能一次升多等
+            }
+        }
+
+        public void go () {
+            setPosition(getPosition() + 1); //走一步，自身位置+1
+        }
+
+        @Override
+        public String toString () {
+            return "姓名: " + getName() + "\n" + "等級: " + getLV() +
+                    "經驗值(當前/最大):" + getExp() + "/" + getMaxExp() +
+                    "各項素質:\n" +
+                    "血量(當前/最大): " + getHp() + "/" + getMaxHp() + "\n" +
+                    "力量: " + getStr() + "\n" +
+                    "敏捷: " + getDex() + "\n" +
+                    "命中: " + getHit() + "\n" +
+                    "防禦: " + getDef() + "\n" +
+                    "裝備數量上限: 武器:" + getWeaponMaxmum() + " 防具:" + getEquipmentMaxmum() +
+                    "背包空間:" + getItemMaxmum() + "負重(當前/最大): "
         }
     }
 }
